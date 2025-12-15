2f36854a146c956ca3608318b9a47bf66aab413f
Jordan Millett <JelloJordan3@gmail.com>
2025-12-14 21:07:11 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   //base: '/salemwdm/', only used for pushing straight to github repo not custom domain
 })
